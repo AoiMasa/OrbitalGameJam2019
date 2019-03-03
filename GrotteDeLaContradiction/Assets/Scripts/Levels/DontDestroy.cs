32892463406fd32089997eda4b2d4d851c9acb3e
Joëlle Perritaz <joelle.perritaz@gmail.com>
2019-03-03 10:46:44 +0100

    Final Level

--- GrotteDeLaContradiction/Assets/Scripts/Levels/DontDestroy.cs
+++ GrotteDeLaContradiction/Assets/Scripts/Levels/DontDestroy.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
+
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
-        if (objs.Length > 1)
-            Destroy(this.gameObject);
+        //if (objs.Length > 2)
+        //    Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject);
 
@@ -18,15 +19,16 @@ public class DontDestroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "FinalLevel")
+        if (SceneManager.GetActiveScene().name == "FinalLevel" && this.name != "MonsterNoise")
         {
             Destroy(this.gameObject);
         }
     }
+
 }
