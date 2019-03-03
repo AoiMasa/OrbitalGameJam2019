61b648a803fcd2e31c28d6c7c0deabc02720be99
Joëlle Perritaz <joelle.perritaz@gmail.com>
2019-03-03 12:51:22 +0100

    Add Stele and end song

---
+++ GrotteDeLaContradiction/Assets/EndGameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private string nextLevel = "MainMenu";
+
+    AudioSource audioSource;
+    public AudioClip otherClip;
+
+    private bool hasPlayedSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        hasPlayedSound = false;
+        audioSource = this.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!hasPlayedSound)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.clip = otherClip;
+                    audioSource.time = 10;
+                    audioSource.Play();
+                    hasPlayedSound = true;
+                    collision.gameObject.GetComponentInParent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+                }
+            }
+           
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (hasPlayedSound)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    SceneManager.LoadScene(nextLevel);
+                }
+            }
+
+
+        }
+    }
+}
