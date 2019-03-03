a318d9ec45ad47cf0e67967d5b7d66062c83e04d
Joëlle Perritaz <joelle.perritaz@gmail.com>
2019-03-03 09:42:21 +0100

    Add graphics for level 0 and 1 Create level 2

---
+++ GrotteDeLaContradiction/Assets/KeyDetectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyDetectionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") || Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
